aff72615944171a175329eb09f26a5cd7fdd139c
ashawni471 <ashwanikumar351999@gmail.com>
2021-03-25 08:17:51 -0700

    initial commit

---
+++ mergesortrecursive.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void mergesort1(int input[],int si,int m)
+{
+    if(input[si]>input[si+1])
+    {
+        i
+    }
+}
+void mergesorting(int input[],int si,int ei)
+{
+    if(si>=ei)
+    {
+        return;
+    }
+    m=si+ei/2;
+    mergesort1(input,si,m);
+}
+
+void mergeSort(int input[], int size){
+
+        mergesorting (input,0,n-1);
+}
+
+int main()
+{
+    int input[100];
+    int n;cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    mergesort(input,n-1);
+
+}
